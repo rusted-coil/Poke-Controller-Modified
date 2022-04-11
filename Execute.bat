3866130938cc00a77c6b5ddd07921645c04b80ac
rusted_coil <rusted.coil@gmail.com>
2022-04-11 17:23:44 +0900

    Execute.bat実行時cdではなくpushdでディレクトリ移動するように修正

--- Execute.bat
+++ Execute.bat
@@ -1,5 +1,6 @@
 rem ŠÂ‹«•Ï” ANACONDA_ROOT: C:\Users\ƒ†[ƒU[\Anaconda3
-cd SerialController
+pushd SerialController
 call %ANACONDA_ROOT%\Scripts\activate.bat
 call activate PokeCon
 python Window.py
+popd
