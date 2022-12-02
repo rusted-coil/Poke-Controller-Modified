c2d9e223a32fd06f1721cfd547f32e92404d7c03
rusted-coil <rusted.coil@gmail.com>
2022-12-02 23:17:14 +0900

    SVマクロ修正

--- SerialController/Commands/MyPythonCommands/DuplicateSV.py
+++ SerialController/Commands/MyPythonCommands/DuplicateSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from Commands.Keys import Button, Hat
+from Commands.Keys import Button, Hat, Direction
 from Commands.PythonCommandBase import PythonCommand
 
 class DuplicateSV(PythonCommand):
@@ -21,70 +21,44 @@ class DuplicateSV(PythonCommand):
     # * ボックスは1から始まるようにしておく
     def duplicate(self):
         # ライドフォルムに戻す
-        self.press(Button.A)
-        self.wait(0.8)
-        self.press(Hat.TOP)
-        self.wait(0.1)
-        self.press(Hat.TOP)
-        self.wait(0.1)
-        self.press(Button.A) # メニュー選択
-        self.wait(2.0)
-        self.press(Button.A) # ライドフォルムにしますか→はい
-        self.wait(0.2)
-        self.press(Button.A)
-        self.wait(0.2)
-        self.press(Button.A)
-        self.wait(0.2)
-        self.press(Button.A)
+        self.press(Button.A, 0.15, 1.0)
+        self.press(Hat.TOP, 0.15, 0.2)
+        self.press(Hat.TOP, 0.15, 0.1)
+        self.press(Button.A, 0.15, 2.0) # メニュー選択
+        self.press(Button.A, 0.15, 0.1) # ライドフォルムにしますか→はい
+        self.press(Button.A, 0.15, 0.1)
+        self.press(Button.A, 0.15, 0.1)
+        self.press(Button.A, 0.15, 0.1)
         self.wait(3.0)
-        self.press(Button.A) # ライドフォルムになった
-        self.wait(0.5)
+        self.press(Button.A, 0.15, 0.5) # ライドフォルムになった
 
         # バトルチームを開く
-        self.press(Hat.RIGHT)
-        self.wait(0.1)
-        self.press(Hat.BTM)
-        self.wait(0.1)
-        self.press(Hat.BTM)
-        self.wait(0.1)
-        self.press(Button.A)
-        self.wait(3.0)
-        self.press(Button.X)
-        self.wait(0.3)
-        self.press(Button.X)
-        self.wait(0.3)
-        self.press(Button.L)
-        self.wait(0.5)
+        self.press(Hat.RIGHT, 0.15, 0.1)
+        self.press(Hat.BTM, 0.15, 0.2)
+        self.press(Hat.BTM, 0.15, 0.1)
+        self.press(Button.A, 0.15, 3.0)
+        self.press(Button.X, 0.3, 0.2)
+        self.press(Button.X, 0.3, 0.2)
+        self.press(Button.L, 0.15, 0.5)
 
         # 道具を預かる
-        self.press(Button.A)
-        self.wait(1.0)
-        self.press(Hat.BTM)
-        self.wait(0.1)
-        self.press(Hat.BTM)
-        self.wait(0.1)
-        self.press(Hat.BTM)
-        self.wait(0.1)
-        self.press(Button.A)
-        self.wait(0.5)
+        self.press(Button.A, 0.15, 1.0)
+        self.press(Hat.BTM, 0.15, 0.1)
+        self.press(Hat.BTM, 0.15, 0.1)
+        self.press(Hat.BTM, 0.15, 0.1)
+        self.press(Button.A, 0.15, 1.0)
 
         # 戻る
-        self.press(Button.B)
-        self.wait(2.5)
-        self.press(Hat.LEFT)
-        self.wait(0.5)
+        self.press(Button.B, 0.15, 2.5)
+        self.press(Hat.LEFT, 0.15, 0.5)
 
     # はまり防止
     def recovery(self):
         for i in range(10):
-            self.press(Button.B)
-            self.wait(0.1)
-        self.press(Button.X)
-        self.wait(1.0)
-        self.press(Hat.RIGHT)
-        self.wait(0.5)
-        self.press(Hat.LEFT)
-        self.wait(0.5)
+            self.press(Button.B, 0.15, 0.1)
+        self.press(Direction.UP, 0.2, 1.0)
+        self.press(Button.X, 0.15, 1.0)
+        self.press(Hat.RIGHT, 0.15, 0.5)
+        self.press(Hat.LEFT, 0.15, 0.5)
         self.press(Hat.TOP, 2.0, 0.6)
-        self.press(Hat.BTM)
-        self.wait(0.5)
+        self.press(Hat.BTM, 0.15, 0.5)
